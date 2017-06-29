a353237aa28674349252b071952c2e8ced24c6cd
Romain Lefrancois <romain.lefrancois@gmail.com>
2017-06-28 21:35:18 -0700

    add aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,14 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+# Easier navigation: .., ..., ...., .....
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ll='ls -la'
+alias h="history"
+alias tailf='tail -f'
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en0"
